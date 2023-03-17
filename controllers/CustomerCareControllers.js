5fc90b5d1b1124dd962838ddefae4dd72c400508
manish <manishvyas393@gmail.com>
2023-03-17 12:20:14 +0530

    sidebar fixed

--- controllers/CustomerCareControllers.js
+++ controllers/CustomerCareControllers.js
@@ -17,7 +17,6 @@ exports.registerCareUser = async (req, res) => {
 exports.login = async (req, res) => {
       try {
             const user = await CustomerCareUser.findByCredentials(req.body.email, req.body.password)
-            console.log(user)
             const token = await user.generateAuthToken()
             res.status(200).cookie("careToken", token).json({
                   success: true,
