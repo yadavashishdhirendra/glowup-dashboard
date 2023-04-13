b7eecdbef783dbc87352530c46fdc016d74d9aed
manish <manishvyas393@gmail.com>
2023-04-13 15:30:28 +0530

    minor changes

--- models/UserModel.js
+++ models/UserModel.js
@@ -11,7 +11,6 @@ const UserModel = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
-        unique: true,
         validate: [validator.isEmail, 'Please Enter a valid Email']
     },
     password: {
